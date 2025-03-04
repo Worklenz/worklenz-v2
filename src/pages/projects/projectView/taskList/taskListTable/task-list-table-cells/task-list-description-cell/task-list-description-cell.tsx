dfac09ab68f96a494337a067c22721ccd6141a91
Danushka <danushka@ceydigital.com>
2025-03-04 09:44:06 +0530

    feat: Sanitize task description to prevent XSS vulnerabilities

--- src/pages/projects/projectView/taskList/taskListTable/task-list-table-cells/task-list-description-cell/task-list-description-cell.tsx
+++ src/pages/projects/projectView/taskList/taskListTable/task-list-table-cells/task-list-description-cell/task-list-description-cell.tsx
@@ -1,13 +1,16 @@
 import { Typography } from 'antd';
 import React from 'react';
+import DOMPurify from 'dompurify';
 
 const TaskListDescriptionCell = ({ description }: { description: string }) => {
+  const sanitizedDescription = DOMPurify.sanitize(description);
   return (
     <Typography.Paragraph
       ellipsis={{ expandable: false }}
       style={{ width: 260, marginBlockEnd: 0 }}
     >
-      {description}
+      {/* {sanitizedDescription} */}
+      <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(sanitizedDescription || '') }} />
     </Typography.Paragraph>
   );
 };
